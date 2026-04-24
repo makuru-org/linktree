719ca6a971eb498b6a73d644296755fcffc67a6f
Makuru <Makuru@Makuru.org>
2026-04-25 00:46:13 +0200

    column centered

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="main">
-      <div className="mx-auto flex flex-column h-[100vh] items-center justify-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <Link href="mailto:makuru@makuru.org">
-          <span className="material-icons-outlined">mail</span>
-        </Link>
+      <div className="mx-auto flex flex-column h-[100vh] justify-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <span className="vertical-align middle">
+          <Link href="mailto:makuru@makuru.org">
+            <span className="text-[24px] inline-middle material-icons-outlined">mail</span>
+          </Link>
+          <Link href="">
+            <span className="text-[24px] si si-matrix"></span>
+          </Link>
+          <Link href="mailto:makuru@makuru.org">
+            <span className="material-icons-outlined">mail</span>
+          </Link>
+          <Link href="mailto:makuru@makuru.org">
+            <span className="material-icons-outlined">mail</span>
+          </Link>
+        </span>
       </div>
-      <Image src="/Spot_the_cow.gif" width={500} height={300} alt="" />
+      <div>
+        <Image src="/Spot_the_cow.gif" width={500} height={300} alt="" />
+      </div>
     </div>
   );
 }
